c5d5ef01d41e8004d41c21db93b203b5bcac6b77
Max Biegert <max@k1te.be>
2019-07-18 18:07:09 +0200

    make tests passing

--- test/tests.js
+++ test/tests.js
@@ -15,7 +15,7 @@ describe('GET /', function() {
     chai.request(app)
     .get('/')
     .end((err, res) => {
-      res.body.should.be.a('string');
+      res.body.should.be.a('object');
       done();
     });
   });
